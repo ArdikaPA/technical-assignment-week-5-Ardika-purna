2f5f8e9fd57605e0193c3c2a33fe737b9d346f87
ArdikaPA <107898643+ArdikaPA@users.noreply.github.com>
2022-07-11 12:45:22 +0700

    Create sensor.py

---
+++ sensor.py
@@ -0,0 +1,9 @@
+#install in terminall
+$ sudo apt-get install git-core
+$ sudo pip3 install Adafruit_DHT
+$ cd Adafruit_Python_DHT
+$ sudo python3 setup.py install
+$ git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+  
+#for call sensor in logic
+import Adafruit_DHT
