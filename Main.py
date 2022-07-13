6475f3da9910e63efd92eccf045f97c10751bf5d
ArdikaPA <107898643+ArdikaPA@users.noreply.github.com>
2022-07-13 10:27:28 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
-import Adafruit_DHT #library untuk memanggil sensor
+import Adafruit_DHT
 
-DHT_SENSOR = Adafruit_DHT.DHT11 #penamaan untuk memanggil data sensor
-DHT_PIN = 4 #penamaan pin input sensor
+DHT_11 = Adafruit_DHT.DHT11
+DHT_PIN = 4
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN) #membaca data yg diambil oleh sensor
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_11, DHT_PIN)
 
     if humidity is not None and temperature is not None: 
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(humidity, temperature)) #menampilkan data sensor dgn format celcius
+        print("Suhu Ruangan={0:0.1f}*C  Kelembapan={1:0.1f}%".format(humidity, temperature))
     else:
-        print("Failed Import Data Sensor") #logic jika gagal mendapatkan data sensor
+        print("Lost Data")
